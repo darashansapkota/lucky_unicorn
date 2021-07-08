1a172178c57e486208c1f25f5d16670b29569fba
darashansapkota <sapkot.darsh@awatapu.school.nz>
2021-07-09 10:30:49 +1200

    times five

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -1,1 +1,8 @@
-print("hello world")
+# Ask users for a number
+get_number = int(input("choose a number"))
+
+# Multiply the number by 5
+times_five = get_number * 5
+
+# Output the result
+print(times_five)
